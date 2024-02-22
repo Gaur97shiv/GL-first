135679d63e68eeadcc6b7a869434ea1073ae02d8
munendra <munendrashiv97@gmail.com>
2024-02-22 09:43:58 +0530

    adding my todays work

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import styles from "./App.module.css";
 import Text from './Components/Text/Text';
-
 const App = () => {
   return (
     <>
     <div className={styles.bg}>
     < Navbar className={styles.navbar} />
-    <img className={styles.bg1} src="src/Components/image/23809-globallogic-office-1.png" alt="background" />
+    <img className={styles.bg1} src="src/Components/image" alt="background" />
     </div>
     <div className={styles.te}>
     <Text/>
@@ -17,3 +16,4 @@ const App = () => {
   )
 }
 export default App;
+
